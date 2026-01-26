8f52be4357479eb9b018276cfffb2935f2bd5b19
Autominds <team@autominds.org>
2026-01-26 16:53:19 -0500

    Add IDE branding and LLM config

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ set -e
 PORT="${PORT:-8080}"
 
 echo "Updating workspace files..."
-cp -r /usr/local/share/workspace/* /home/coder/project/
+cp -r /usr/local/share/workspace/. /home/coder/project/
 
 echo "Starting Autominds IDE on port $PORT..."
 exec code-server --bind-addr "0.0.0.0:$PORT" --auth none /home/coder/project /home/coder/project/README.md
